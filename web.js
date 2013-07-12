aee11b5b1b1cbc5fedc210cf2945f9ba439c95cc
Antonio Herraiz <herraiz@gmail.com>
2013-07-12 03:07:24 +0000

    web app contents read from index.html

--- web.js
+++ web.js
@@ -2,8 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+// HW3
+var fs = require('fs');
+var indexContent = fs.readFileSync("index.html").toString(); // converts buffer to utf8 string
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+  response.send(indexContent);
 });
 
 var port = process.env.PORT || 5000;
